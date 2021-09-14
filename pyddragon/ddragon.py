ca63aef2623a60cbb76b66d705e49ff48e89bac1
dp0973 <yeong0973@gmail.com>
2021-09-15 04:12:26 +0900

    feat(ddragon): add item

--- pyddragon/ddragon.py
+++ pyddragon/ddragon.py
@@ -1,48 +1,86 @@
-from typing import Optional, Union
+from typing import Literal, Optional, Union
 
-from pyddragon.models import Champion
+from pyddragon.models import Champion, Item
 from pyddragon.request import DdragonHttpClient
 
 
 class Ddragon:
     def __init__(self, lang: Optional[str] = "en_US") -> None:
-        self.http = DdragonHttpClient()
+        self.request = DdragonHttpClient()
         self.lang = lang
-        self.loaded = False
-
-    def set_ch_data(self, ch_data: dict[str, str]) -> dict[str, str]:
-        data = {}
-        for key in self.ch_keys:
-            data[key] = ch_data[key]
-
-        return Champion(data)
+        self.ch_loaded = False
+        self.it_loaded = False
+        self.ru_loaded = False
+        self.su_loaded = False
 
     async def load_champion(self) -> None:
         self.ch_by_name = {}
         self.ch_by_key = {}
         self.ch_by_id = {}
-        self.ch_keys = ["name", "id", "key", "title", "blurb", "image", "tags"]
+        ch_props = ["name", "id", "key", "title", "blurb", "image", "tags"]
 
-        chs_json = await self.http.get_base_json("champion", self.lang)
+        chs_json = await self.request.get_base_json("champion", self.lang)
         for ch in chs_json["data"]:
             ch_json = chs_json["data"][ch]
-            ch_data = self.set_ch_data(ch_json)
+
+            data = {}
+            for prop in ch_props:
+                if prop == "image":
+                    data[prop] = ch_json[prop]["full"]
+                    continue
+
+                data[prop] = ch_json[prop]
+
+            ch_data = Champion(data)
 
             self.ch_by_name[ch_json["name"]] = ch_data
             self.ch_by_id[ch_json["id"]] = ch_data
-            self.ch_by_key[ch_json["key"]] = ch_data
+            self.ch_by_key[int(ch_json["key"])] = ch_data
 
-    async def load_all(self) -> None:
-        await self.load_champion()
-        # await self.load_item()
-        # await self.load_summoner()
-        # await self.load_rune()
+        self.ch_loaded = True
 
-        self.loaded = True
+    async def load_item(self) -> None:
+        self.it_by_name = {}
+        self.it_by_id = {}
+        it_keys = [
+            "name",
+            "id",
+            "colloq",
+            "plaintext",
+            "image",
+            "gold",
+            "tags",
+            "stats",
+        ]
 
-    async def get_champion(self, query: Union[str, int], type: Optional[str] = "id"):
-        if not self.loaded:
-            await self.load_all()
+        its_json = await self.request.get_base_json("item", self.lang)
+        for it in its_json["data"]:
+            it_json = its_json["data"][it]
+
+            data = {}
+            for prop in it_keys:
+                if prop == "id":
+                    data[prop] = it
+                    continue
+
+                elif prop == "image":
+                    data[prop] = it_json[prop]["full"]
+                    continue
+
+                data[prop] = it_json[prop]
+
+            it_data = Item(data)
+
+            self.it_by_name[it_json["name"]] = it_data
+            self.it_by_id[int(it)] = it_data
+
+        self.it_loaded = True
+
+    async def get_champion(
+        self, query: Union[str, int], type: Literal["id", "name"] = "id"
+    ) -> Champion:
+        if not self.ch_loaded:
+            await self.load_champion()
 
         if isinstance(query, int):
             return self.ch_by_key[query]
@@ -53,3 +91,14 @@ class Ddragon:
             return self.ch_by_id[query]
 
         return self.ch_by_name[query]
+
+    async def get_item(self, query: Union[str, int]) -> Item:
+        if not self.it_loaded:
+            await self.load_item()
+
+        if isinstance(query, int):
+            return self.it_by_id[query]
+        if query.isdigit():
+            return self.it_by_id[int(query)]
+
+        return self.it_by_name[query]
--- pyddragon/models.py
+++ pyddragon/models.py
@@ -3,11 +3,11 @@ class Champion:
         self.data = {
             "name": data["name"],  # Aurelion Sol
             "id": data["id"],  # AurelionSol
-            "key": data["key"],  # 266
+            "key": data["key"],  # "266"
             "title": data["title"],  # The Star Forger
             "blurb": data["blurb"],  # Aurelion Sol once graced...
             "image": data["image"],  # AurelionSol.png
-            "tags": data["tags"],  # [Mage]
+            "tags": data["tags"],  # ["Mage"]
         }
 
     @property
@@ -37,3 +37,51 @@ class Champion:
     @property
     def tags(self):
         return self.data["tags"]
+
+
+class Item:
+    def __init__(self, data: dict):
+        self.data = {
+            "name": data["name"],  # Perfectly Timed Stopwatch
+            "id": data["id"],  # "2423"
+            "colloq": data["colloq"],  # ;zhg;zonyas
+            "plaintext": data["plaintext"],  # Activate to become...
+            "image": data["image"],  # 2423.png
+            "gold": data[
+                "gold"
+            ],  # {"base": 650, "purchasable": False, "total": 650, "sell": 260}
+            "tags": data["tags"],  # ["Active"]
+            "stats": data["stats"],  # Fake: {"FlatHPPoolMod": 200}
+        }
+
+    @property
+    def name(self):
+        return self.data["name"]
+
+    @property
+    def id(self):
+        return self.data["id"]
+
+    @property
+    def colloq(self):
+        return self.data["colloq"]
+
+    @property
+    def plaintext(self):
+        return self.data["plaintext"]
+
+    @property
+    def image(self):
+        return self.data["image"]
+
+    @property
+    def gold(self):
+        return self.data["gold"]
+
+    @property
+    def tags(self):
+        return self.data["tags"]
+
+    @property
+    def stats(self):
+        return self.data["stats"]
